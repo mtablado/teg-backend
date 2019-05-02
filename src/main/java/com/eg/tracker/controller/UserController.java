41b5dc1c0f8ef3f80bccf560090b7adfadb0adac
Miguel Tablado <miguel.tablado.leon@ibm.com>
2019-05-02 19:42:04 +0200

    Current user endpoint

--- src/main/java/com/eg/tracker/controller/UserController.java
+++ src/main/java/com/eg/tracker/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,6 +39,12 @@ public class UserController {
 	@RequestMapping(value="/users/username/{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody User getUserByUsername(@PathVariable("username") String username) throws Exception {
 		return this.service.findUserByUsername(username);
+	}
+
+	@ResponseStatus(value = HttpStatus.OK)
+	@RequestMapping(value="/users/current-user", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody User getCurrentUser() throws Exception {
+		return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 	}
 
 }
